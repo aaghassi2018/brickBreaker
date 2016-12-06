444f1504add8de3a9cf38a9c3bb34495e1602786
Ashkon Aghassi <aaghassi2018@francisparker.org>
2016-12-06 12:10:40 -0800

    no message

--- brickBreaker.js
+++ brickBreaker.js
@@ -1,17 +1,30 @@
 var canvasGA = document.getElementById("gameArea");
 var contextGA = canvasGA.getContext("2d");
 window.addEventListener('keydown', processKeyboardInput);
+window.addEventListener('keyup', processKeyUp);
 
-var gameInSession = true;
+var gameInSession = false;
 
 
 var bar = new Bar();
+var ball = new Ball();
 
+var left = false;
+var right =false;
 
-init();
 
 function init(){
-  bar.draw(90);
+  bar.x = 90;
+  bar.y = canvasGA.height-bar.height;
+
+  ball.x = 0;
+  ball.y = canvasGA.height-200;
+  ball.vx=0.5;
+  ball.vy=0.5;
+
+  gameInSession=true;
+  bar.draw();
+  ball.draw();
 }
 
 setInterval( playGame, 1 );
@@ -26,14 +39,22 @@ function processKeyboardInput(event) {
 	var key = event.key;
 
 	if(key == 'a'){
-    if(bar.x>= -25){
-		  bar.x -= 14;
-    }
+     left = true;
   }
 	else if(key == 'd'){
-    if(bar.x<=canvasGA.width-59){
-		  bar.x += 14;
-    }
+     right = true;
+  }
+
+}
+
+function processKeyUp(event) {
+  var key = event.key;
+
+  if(key == 'a'){
+     left = false;
+  }
+  else if(key == 'd'){
+     right = false;
   }
 }
 
@@ -42,7 +63,27 @@ function update() {
 	if(!gameInSession)
 		return;
 
+    if(ball.y >= canvasGA.height - bar.height-20 && ball.x>bar.x && (ball.x + ball.width) <(bar.x+bar.width)){
+      ball.vy *= -1;
+    }
+    move();
+    ball.update();
     drawStuff();
+
+
+}
+
+function move() {
+  if(right) {
+    if(bar.x<canvasGA.width - bar.width) {
+      bar.x += 2;
+    }
+  }
+  if(left) {
+    if(bar.x>0) {
+      bar.x -= 2;
+    }
+  }
 }
 
 function drawStuff() {
@@ -51,6 +92,7 @@ function drawStuff() {
 
 
 	bar.draw();
+  ball.draw();
 }
 
 
@@ -59,14 +101,35 @@ function processUserInput(event) {
 }
 
 function Bar() {
-	this.width = 80;
+	this.width = 100;
 	this.height = 20;
 	this.x = 90;
   this.y = canvasGA.height-this.height;
 
   this.draw = function() {
       contextGA.fillStyle = "#FF00FF";
-  			contextGA.fillRect(this.x, this.y, this.width, this.height);
-  		}
+  		contextGA.fillRect(this.x, this.y, this.width, this.height);
+  }
 
+}
+
+function Ball() {
+  this.width = 20;
+	this.height = 20;
+
+	this.x = 0;
+  this.y = canvasGA.height-200;
+
+  this.vx=0.5;
+  this.vy=0.5;
+
+  this.draw = function() {
+    contextGA.fillStyle = "#50FFAB"
+    contextGA.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  this.update = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
 }
