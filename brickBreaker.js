3a213d091659ce1ba0cc98bc73b255163d4b070d
Ashkon Aghassi <aaghassi2018@francisparker.org>
2017-01-11 15:04:40 -0800

    made the bricks disappear when hit, started working on level 2...

--- brickBreaker.js
+++ brickBreaker.js
@@ -11,8 +11,9 @@ var ball = new Ball();
 var bricks = [];
 
 var level1Cords = [125, 60, 55, 150, 55, 270, 125, 370, 325, 60, 395, 150, 395, 270, 325, 370];
-var gameMode;
+var level2Cords = [50,100,100,25,25,2,5,55,55,6,55,44,874,758,250,250];
 
+var workingCords = [];
 
 
 var left = false;
@@ -22,10 +23,15 @@ var once = 0;
 
 
 function init1(){
-  gameMode = 'norm';
-  for (var i = 0; i < level1Cords.length; i +=2) {
-    bricks.push(new Brick(level1Cords[i], level1Cords[i+1]));
+
+  workingCords = level1Cords;
+
+  for (var i = 0; i < workingCords.length; i +=2) {
+    bricks.push(new Brick(workingCords[i], workingCords[i+1]));
   }
+
+
+
   bar.x = 90;
   bar.y = canvasGA.height-bar.height;
 
@@ -38,9 +44,29 @@ function init1(){
   gameInSession=true;
   bar.draw();
   ball.draw();
-  bricks.forEach(function(element) {
-    element.draw();
-  });
+
+}
+
+function init2(){
+  workingCords = level2Cords;
+  for (var i = 0; i < workingCords.length; i +=2) {
+    bricks.push(new Brick(workingCords[i], workingCords[i+1]));
+  }
+
+
+
+  bar.x = 90;
+  bar.y = canvasGA.height-bar.height;
+
+  ball.x = 0;
+  ball.y = canvasGA.height-200;
+  ball.vx=5;
+  ball.vy=5;
+
+
+  gameInSession=true;
+  bar.draw();
+  ball.draw();
 }
 
 setInterval( playGame, 33 );
@@ -88,10 +114,12 @@ function update() {
       ball.vy *=-1;
     }
 
-    for(var i = 0; i < level1Cords.length; i +=2){
-      if(ball.x + ball.width >= level1Cords[i] && ball.x <= level1Cords[i]+ 40){
-        if(ball.y + ball.height >= level1Cords[i+1] && ball.y <= level1Cords[i+1] + 20){
-          bro(level1Cords[i],level1Cords[i+1]);
+    for(var i = 0; i < workingCords.length; i +=2){
+      if(ball.x + ball.width >= workingCords[i] && ball.x <= workingCords[i]+ 40){
+        if(ball.y + ball.height >= workingCords[i+1] && ball.y <= workingCords[i+1] + 20){
+          bro(workingCords[i],workingCords[i+1]);
+          workingCords.splice(i,2);
+          bricks.splice(i/2,1);
         }
       }
     }
@@ -112,7 +140,6 @@ function bro(x,y){
 for(once; once< 1; once++){
   if(ball.x >= x && ball.x < x+40){
     ball.vy*= -1;
-
 
     }
     else if (ball.y> y) {
@@ -149,10 +176,11 @@ function drawStuff() {
 
 	bar.draw();
   ball.draw();
-  bricks.forEach(function(element) {
-    element.draw();
-  })
 
+
+  bricks.forEach(function(brick) {
+    brick.draw();
+  })
 }
 
 
