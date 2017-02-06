c7eca2d411c1ec5cef35c3d25f8cebd784e6a8f8
Ashkon Aghassi <aaghassi2018@francisparker.org>
2017-02-06 10:31:00 -0800

    changed cookies to localStorage… everything works :)

--- brickBreaker.js
+++ brickBreaker.js
@@ -8,17 +8,31 @@ var gameInSession = false;
 
 var bar = new Bar();
 var ball = new Ball();
+//normal mode
 var bricks = [];
 var darkBricks = [];
 
-var level1Cords = [125, 60, 55, 150, 55, 270, 125, 370, 325, 60, 395, 150, 395, 270, 325, 370];
-var level2Cords = [50,50,228,369,456,214,352,145,214,454];
-var level3Cords = [350,350,100,100,200,200,300,300];
-var level3CordsDark = [98,98];
+//special mode
+var shortPaddleBricks = [];
 
+var level1Cords = [125, 60, 55, 150, 55, 270, 125, 370, 325, 60, 395, 150, 395, 270, 325, 370];
+
+
+var level2Cords = [50,50,228,369,456,214,352,145,214,454];
+
+
+var level3Cords = [350,350,100,100,200,200,300,300];
+var level3CordsDark = [300,300];
+
+//special mode
+var level1SCords = [100,100];
+var level1SCordsShortPaddle = [300,300];
 
 var workingCords = [];
 var workingCordsDark = [];
+
+//special mode
+var workingCordsShortPaddle = [];
 
 var left = false;
 var right =false;
@@ -29,28 +43,18 @@ var l1;
 var l2;
 var l3;
 
-//from w3schools.com
+var l1S;
+
+
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
+  return localStorage.getItem(cname);
 }
 
-//from w3schools.com
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+function setCookie(cname, cvalue) {
+
+  localStorage.setItem(cname,cvalue);
+
 }
 
 function checkProgress(){
@@ -60,35 +64,47 @@ function checkProgress(){
   l2 = getCookie("l2");
   l3 = getCookie("l3");
 
-    if (l1 == null) {
-      l1 = 0;
-    }
-    if (l2 == null) {
-      l2 = 0;
-    }
-    if (l3 == null) {
-      l3 = 0;
+  l1S = getCookie("l1S");
 
-    }
+  if (l1 == null) {
+    l1 = '0';
+  }
+  if (l2 == null) {
+    l2 = '0';
+  }
+  if (l3 == null) {
+    l3 = '0';
 
-    if(l1 == 1){
-      document.getElementById("l1B").style.backgroundColor = "#00FF00";
-    }
-    else {
-      document.getElementById("l1B").style.backgroundColor = "#FF0000";
-    }
-    if(l2 == 1){
-      document.getElementById("l2B").style.backgroundColor = "#00FF00";
-    }
-    else {
-      document.getElementById("l2B").style.backgroundColor = "#FF0000";
-    }
-    if(l3 == 1){
-      document.getElementById("l3B").style.backgroundColor = "#00FF00";
-    }
-    else {
-      document.getElementById("l3B").style.backgroundColor = "#FF0000";
-    }
+  }
+  if (l1S == null) {
+    l1S = '0';
+
+  }
+
+  if(l1 == 1){
+    document.getElementById("l1B").style.backgroundColor = "#00FF00";
+  }
+  else {
+    document.getElementById("l1B").style.backgroundColor = "#FF0000";
+  }
+  if(l2 == 1){
+    document.getElementById("l2B").style.backgroundColor = "#00FF00";
+  }
+  else {
+    document.getElementById("l2B").style.backgroundColor = "#FF0000";
+  }
+  if(l3 == 1){
+    document.getElementById("l3B").style.backgroundColor = "#00FF00";
+  }
+  else {
+    document.getElementById("l3B").style.backgroundColor = "#FF0000";
+  }
+  if(l1S == 1){
+    document.getElementById("l1SB").style.backgroundColor = "#00FF00";
+  }
+  else {
+    document.getElementById("l1SB").style.backgroundColor = "#FF0000";
+  }
 }
 
 function init1(){
@@ -150,9 +166,41 @@ function init3(){
   for (var i = 0; i < workingCords.length; i +=2) {
     bricks.push(new Brick(workingCords[i], workingCords[i+1]));
   }
-  for (var i = 0; i < workingCords.length; i +=2) {
+  for (var i = 0; i < workingCordsDark.length; i +=2) {
     darkBricks.push(new darkBrick(workingCordsDark[i], workingCordsDark[i+1]));
   }
+  document.getElementById("rightOfGameArea").style.visibility = "hidden";
+
+
+  bar.x = 90;
+  bar.y = canvasGA.height-bar.height;
+
+  ball.x = 0;
+  ball.y = canvasGA.height-200;
+  ball.vx=6;
+  ball.vy=6;
+
+
+
+  gameInSession=true;
+  bar.draw();
+  ball.draw();
+
+}
+
+function init1S(){
+  workingCords = level1SCords;
+  workingCordsShortPaddle = level1SCordsShortPaddle;
+
+  currL = 'l1S';
+  for (var i = 0; i < workingCords.length; i +=2) {
+    bricks.push(new Brick(workingCords[i], workingCords[i+1]));
+  }
+  for (var i = 0; i < workingCordsShortPaddle.length; i +=2) {
+    shortPaddleBricks.push(new shortPaddleBrick(workingCordsShortPaddle[i], workingCordsShortPaddle[i+1]));
+  }
+
+
   document.getElementById("rightOfGameArea").style.visibility = "hidden";
 
 
@@ -175,20 +223,19 @@ function init3(){
 
 setInterval( playGame, 33 );
 function playGame() {
-	if(!gameInSession)
-		return;
-
+  if(!gameInSession)
+  return;
 
   update();
 }
 
 function processKeyboardInput(event) {
-	var key = event.keyCode;
-	if(key == 37 || key == 65){
-     left = true;
+  var key = event.keyCode;
+  if(key == 37 || key == 65){
+    left = true;
   }
-	else if(key == 39 || key == 68 ){
-     right = true;
+  else if(key == 39 || key == 68 ){
+    right = true;
   }
 
 }
@@ -196,150 +243,125 @@ function processKeyboardInput(event) {
 function processKeyUp(event) {
   var key = event.keyCode;
   if(key == 37 || key == 65){
-     left = false;
+    left = false;
   }
   else if(key == 39 || key == 68){
-     right = false;
+    right = false;
   }
 }
 
 function update() {
 
-	if(!gameInSession)
-		return;
+  if(!gameInSession)
+  return;
   if(workingCords.length == 0){
     win();
 
   }
 
-    if(ball.y >= canvasGA.height - bar.height-20 && ball.x+ball.width>bar.x && ball.x <(bar.x+bar.width)){
-      //ball collision with bar
-      if(right==true){
-          // if(ball.vx>=0){
-          //   //window.alert('ball going right, bar going right')
-          //   ball.vy*=-1;
-          //
-          //
-          //   //ball.vx+=1;
-          //   ball.vy-=1;
-          // }
-          // else {
-          //   //window.alert('ball going left, bar going right')
-          //   ball.vy*=-1;
-          //
-          //
-          //   //ball.vx-=1;
-          //   ball.vy+=1;
-          // }
-          ball.vy *=-1;
-          ball.vx+=1;
-      }
-      else if(left==true){
-        // if(ball.vx<=0){
-        //   //window.alert('ball going left, bar going left')
-        //   ball.vy*=-1;
-        //
-        //
-        //   ball.vx+=2;
-        //   ball.vy+=2;
-        // }
-        // else {
-        //   //window.alert('ball going right, bar going left')
-        //   //window.alert('yo')
-        //   ball.vy*=-1;
-        //
-        //
-        //   ball.vx-=1;
-        //   ball.vy-=1;
-        // }
-        ball.vy *=-1;
-        ball.vy-=1;
-      }
-      else {
-        {
-          ball.vy *=-1;
-        }
-      }
-
-
-    }
-    if(ball.vx>= 7){
-      ball.vx = 7;
-    }
-    if(ball.vy>= 7){
-      ball.vy = 7;
-    }
-    if(ball.x <= -2 || ball.x+ball.width >= canvasGA.width+2){
-      ball.vx*=-1;
-    }
-    if(ball.y <= -2){
+  if(ball.y >= canvasGA.height - bar.height-20 && ball.x+ball.width>bar.x && ball.x <(bar.x+bar.width)){
+    //ball collision with bar
+    if(right==true){
       ball.vy *=-1;
+      ball.vx+=1;
     }
-
-    for(var i = 0; i < workingCords.length; i +=2){
-      if(ball.x + ball.width >= workingCords[i] && ball.x <= workingCords[i]+ 40){
-        if(ball.y + ball.height >= workingCords[i+1] && ball.y <= workingCords[i+1] + 20){
-          bro(workingCords[i],workingCords[i+1]);
-          workingCords.splice(i,2);
-          bricks.splice(i/2,1);
-
-        }
+    else if(left==true){
+      ball.vy *=-1;
+      ball.vy-=1;
+    }
+    else {
+      {
+        ball.vy *=-1;
       }
     }
-    for(var i = 0; i < workingCordsDark.length; i +=2){
-      if(ball.x + ball.width >= workingCordsDark[i] && ball.x <= workingCordsDark[i]+ 40){
-        if(ball.y + ball.height >= workingCordsDark[i+1] && ball.y <= workingCordsDark[i+1] + 20){
-          bro(workingCordsDark[i],workingCordsDark[i+1]);
-          workingCordsDark.splice(i,2);
-          darkBricks.splice(i/2,1);
 
-        }
+
+  }
+  if(ball.vx>= 7){
+    ball.vx = 7;
+  }
+  if(ball.vy>= 7){
+    ball.vy = 7;
+  }
+  if(ball.x <= -2 || ball.x+ball.width >= canvasGA.width+2){
+    ball.vx*=-1;
+  }
+  if(ball.y <= -2){
+    ball.vy *=-1;
+  }
+
+  for(var i = 0; i < workingCords.length; i +=2){
+    if(ball.x + ball.width >= workingCords[i] && ball.x <= workingCords[i]+ 40){
+      if(ball.y + ball.height >= workingCords[i+1] && ball.y <= workingCords[i+1] + 20){
+        bro(workingCords[i],workingCords[i+1]);
+        workingCords.splice(i,2);
+        bricks.splice(i/2,1);
+
       }
     }
-    once = 0;
+  }
+  for(var i = 0; i < workingCordsDark.length; i +=2){
+    if(ball.x + ball.width >= workingCordsDark[i] && ball.x <= workingCordsDark[i]+ 45){
+      if(ball.y + ball.height >= workingCordsDark[i+1] && ball.y <= workingCordsDark[i+1] + 25){
+        bro(workingCordsDark[i],workingCordsDark[i+1]);
+        workingCordsDark.splice(i,2);
+        darkBricks.splice(i/2,1);
 
-
-    move();
-    ball.update();
-    drawStuff();
-
-    if(ball.y >= canvasGA.height - bar.height){
-      gameOver();
-      gameInSession = false;
+      }
     }
+  }
+  for(var i = 0; i < workingCordsShortPaddle.length; i +=2){
+    if(ball.x + ball.width >= workingCordsShortPaddle[i] && ball.x <= workingCordsShortPaddle[i]+ 45){
+      if(ball.y + ball.height >= workingCordsShortPaddle[i+1] && ball.y <= workingCordsShortPaddle[i+1] + 25){
+        bro(workingCordsShortPaddle[i],workingCordsShortPaddle[i+1]);
+        workingCordsShortPaddle.splice(i,2);
+        shortPaddleBricks.splice(i/2,1);
+        bar.width = 50;
+      }
+    }
+  }
+  once = 0;
+
+
+  move();
+  ball.update();
+  drawStuff();
+
+  if(ball.y >= canvasGA.height - bar.height){
+    gameOver();
+    gameInSession = false;
+  }
 }
 
 function win(){
   if(currL == 1){
-    //document.cookie = "l1=1";
-    setCookie('l1',1,365);
+    setCookie('l1','1');
   }
   if(currL == 2){
-    //document.cookie = "l2=1";
-    setCookie('l2',1,365);
+    setCookie('l2','1');
   }
   if(currL == 3){
-    //document.cookie = "l3=1";
-    setCookie('l3',1,365);
-
+    setCookie('l3','1');
   }
+  if(currL == 'l1S'){
+    setCookie('l1S','1');
+  }
+  checkProgress()
   document.getElementById("youWinDiv").style.visibility='visible';
   document.getElementById("gameOn").style.visibility='hidden';
   gameInSession=false;
 }
 
 function bro(x,y){
-for(once; once< 1; once++){
+  for(once; once< 1; once++){
 
-  if(ball.x >= x && ball.x < x+40){
-    ball.vy*= -1;
+    if(ball.x <= x || ball.x+ball.width > x+45){
+      console.log('hit the bottom or top');
+      ball.vx*= -1;
     }
-    else if (ball.y> y) {
+    else if (ball.y < y || ball.y + ball.height > y + 20) {
       ball.vy*=-1;
-    }
-    else
-    {
-      ball.vx *= -1;
+      console.log('hit the side');
     }
   }
 }
@@ -366,10 +388,10 @@ function move() {
 
 function drawStuff() {
   contextGA.fillStyle = "#FFFFFF";
-	contextGA.fillRect(0, 0, canvasGA.width, canvasGA.height);
+  contextGA.fillRect(0, 0, canvasGA.width, canvasGA.height);
 
 
-	bar.draw();
+  bar.draw();
   ball.draw();
 
 
@@ -379,20 +401,23 @@ function drawStuff() {
   darkBricks.forEach(function(darkBrick) {
     darkBrick.draw();
   })
+  shortPaddleBricks.forEach(function(shortBrick) {
+    shortBrick.draw();
+  })
 }
 
 
 
 
 function Bar() {
-	this.width = 100;
-	this.height = 20;
-	this.x = 90;
+  this.width = 100;
+  this.height = 20;
+  this.x = 90;
   this.y = canvasGA.height-this.height;
 
   this.draw = function() {
-      contextGA.fillStyle = "#FF00FF";
-  		contextGA.fillRect(this.x, this.y, this.width, this.height);
+    contextGA.fillStyle = "#FF00FF";
+    contextGA.fillRect(this.x, this.y, this.width, this.height);
   }
 
 }
@@ -404,8 +429,21 @@ function Brick(x, y) {
   this.y = y;
 
   this.draw = function() {
-      contextGA.fillStyle = "#FF0000";
-      contextGA.fillRect(this.x, this.y, this.width, this.height);
+    contextGA.fillStyle = "#FF0000";
+    contextGA.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+}
+
+function shortPaddleBrick(x, y) {
+  this.width = 40;
+  this.height = 20;
+  this.x = x;
+  this.y = y;
+
+  this.draw = function() {
+    contextGA.fillStyle = "#000000";
+    contextGA.fillRect(this.x, this.y, this.width, this.height);
   }
 
 }
@@ -417,18 +455,18 @@ function darkBrick(x, y) {
   this.y = y;
 
   this.draw = function() {
-      contextGA.fillStyle = "#990000";
-      contextGA.fillRect(this.x, this.y, this.width, this.height);
+    contextGA.fillStyle = "#990000";
+    contextGA.fillRect(this.x, this.y, this.width, this.height);
   }
 
 }
 
 function Ball() {
   this.width = 20;
-	this.height = 20;
+  this.height = 20;
 
 
-	this.x = 0;
+  this.x = 0;
   this.y = canvasGA.height-200;
 
   this.vx=0;
